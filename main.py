6c6bcceaa2cb2d724dede102b45e4052c9644f30
kimyoungho010311 <kim246464@naver.com>
2025-06-14 22:14:17 +0900

    feat : 예외처리 라이브러리 추가 임포트

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from script.crawling.chosun_ilbo import crawling_chosun
 
 def main():
     # 조선일보 뉴스기사 수집.
-    crawling_chosun(2)
+    crawling_chosun(400)
 
 
 if __name__ == '__main__':
--- script/crawling/chosun_ilbo.py
+++ script/crawling/chosun_ilbo.py
@@ -4,6 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    TimeoutException,
+    WebDriverException,
+    ElementClickInterceptedException,
+    StaleElementReferenceException
+)
+
 
 import csv
 import time

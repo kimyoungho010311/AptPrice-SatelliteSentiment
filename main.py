16dff0744549cc7541d10aa88ab69926b5ce5974
kimyoungho010311 <kim246464@naver.com>
2025-06-15 21:52:54 +0900

    feat : 조선일보 전처리 함수화
    TODO : 동아일보 전처리 함수 구현
    
    chore : 중간단계 전처리 데이터 저장하는 interim 디렉토리 생성

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+# 뉴스 크롤링
 from script.crawling.chosun_ilbo import crawling_chosun
 from script.crawling.dong_a_ilbo import crawling_dong_a
+
+# 크롤링 뉴스 전처리
+from script.prepro_news.prepro_chosun import prepro_chosun
 
 def main():
     # 조선일보 뉴스기사 수집.
     #crawling_chosun(400)
     # 400까지 해봐?
-    crawling_dong_a(1)
+    #crawling_dong_a(1)
+    prepro_chosun()
 # 가상환경으로 시작하자!!!
 # soruce ~~
 if __name__ == '__main__':
---
+++ script/prepro_news/prepro_chosun.py
@@ -0,0 +1,24 @@
+def prepro_chosun():
+    import pandas as pd
+    SAVE_PATH = 'data/interim/news/interim_chosun.csv'
+
+    chosun = pd.read_csv("data/raw/news/chosun_ilbo.csv")
+
+    # 결측 기사는 전부 삭제함
+    chosun.dropna(inplace=True)
+
+    # URL 기준으로 날짜 추출함
+    chosun['date'] = pd.to_datetime(chosun['URL'].apply(lambda x: "-".join(x.split("/")[5:8])))
+
+    # 날짜 순으로 정렬후 URL 삭제
+    chosun.sort_values(by='date',ascending=False, inplace=True)
+    chosun.drop('URL', axis=1, inplace=True)
+    # 컬럼 순서 변경
+    chosun = chosun[['date','content']]
+    # 인덱스 번호 초기화
+    chosun = chosun.reset_index(drop=True)
+
+    chosun.to_csv(SAVE_PATH, index=False)
+    print(f"Interim_chosun_ilbo.csv saved at {SAVE_PATH}")
+    
+    return 

a9693aa9021a01da97f05a0c7b8d6776afb35179
kimyoungho010311 <kim246464@naver.com>
2025-06-16 20:10:16 +0900

    feat : 중앙일보 크롤링 기능 구현 완료.
    TODO : 중앙일보 크롤링 기능 함수화 필요

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from script.prepro_news.prepro_dong_a import prepro_dong_a
 def main():
     # 조선일보 뉴스기사 수집.
     #crawling_chosun(400)
-    # 400까지 해봐?
-    #crawling_dong_a(1)
     #prepro_chosun()
+    # 400까지?
+    crawling_dong_a(400)
     prepro_dong_a()
-# 가상환경으로 시작하자!!!
+
 # soruce ~~
 if __name__ == '__main__':
     main()
